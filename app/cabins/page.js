5d93117a8d1d56ff92dc3a3bf411d317bf23d8fc
Robin de Villers <rdevilliers@adaptavist.com>
2025-04-19 13:28:35 +0200

    set up layout

---
+++ app/cabins/page.js
@@ -0,0 +1,9 @@
+import Navigation from "../components/Navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
   return (
-    <h1>Hello Next!</h1>
-  )
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
