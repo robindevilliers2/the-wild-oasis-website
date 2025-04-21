27a1cad6404e1a8fba482efaa960d5d00761354d
Robin de Villers <rdevilliers@adaptavist.com>
2025-04-21 13:06:29 +0200

    configure page meta data

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About",
+};
+
 export default function Page() {
-  return <h1>About the Wild Oasis</h1>
+  return <h1>About the Wild Oasis</h1>;
 }
--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Guest Area",
+};
+
 export default function Page() {
-  return <h1>Your account</h1>
+  return <h1>Your account</h1>;
 }
