fa51ecf0a79eb74762c06cf9759bcbcf4f045f94
Robin de Villers <rdevilliers@adaptavist.com>
2025-04-21 12:45:13 +0200

    start project structure

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
 
 export const metadata = {
   title: "The Wild Oasis",
-}
+};
 
-export default function RootLayout({children}){
-  return <html lang="en">
-    <body>
-      <header><Logo /></header>
-      <Navigation />
-      <main>{children}</main>
-      <footer>Copyright by The Wild Oasis</footer>
-    </body>
-  </html> 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <Logo />
+        </header>
+        <Navigation />
+        <main>{children}</main>
+        <footer>Copyright by The Wild Oasis</footer>
+      </body>
+    </html>
+  );
 }
