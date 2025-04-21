c85fc7531a983f813c44d3289a1ce6ec61bd8992
Robin de Villers <rdevilliers@adaptavist.com>
2025-04-21 14:08:10 +0200

    optimise images and create home page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -15,13 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} relative flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
