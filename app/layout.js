ab06b857e020580196d3058c186bea159dec7e17
Robin de Villers <rdevilliers@adaptavist.com>
2025-04-21 15:20:52 +0200

    implement nested guest area navigation

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} relative flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="mx-auto max-w-7xl">{children}</main>
+        <div className="grid flex-1 px-8 py-12">
+          <main className="mx-auto w-full max-w-7xl">{children}</main>
         </div>
       </body>
     </html>
