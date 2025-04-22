c3b7fc3bc844815fe5f3a94599639f2540a54816
Robin de Villers <rdevilliers@adaptavist.com>
2025-04-22 10:31:01 +0200

    fetch cabin data

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      new URL(
+        `${process.env.SUPABASE_URL}/storage/v1/object/public/cabin-images/**`,
+      ),
+    ],
+  },
+};
 
 export default nextConfig;
