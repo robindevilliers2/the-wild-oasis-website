258a2ce0f5d38da2bb18ff88764ed9e32d2e1fd1
robindevilliers2 <robindevilliers2@gmail.com>
2025-05-03 08:15:47 +0200

    Add reservation forms

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,6 +1,6 @@
 import SelectCountry from "@/app/_components/SelectCountry";
 import SpinnerMini from "@/app/_components/SpinnerMini";
-import Image from "next/image";
+import UpdateProfileForm from "@/app/_components/UpdateProfileForm";
 import { Suspense } from "react";
 
 export const metadata = {
@@ -8,10 +8,8 @@ export const metadata = {
 };
 
 export default function Page() {
-  // CHANGE
   const countryFlag = "pt.jpg";
   const nationality = "portugal";
-
   return (
     <div>
       <h2 className="mb-4 text-2xl font-semibold text-accent-400">
@@ -22,57 +20,14 @@ export default function Page() {
         Providing the following information will make your check-in process
         faster and smoother. See you soon!
       </p>
-
-      <form className="flex flex-col gap-6 bg-primary-900 px-12 py-8 text-lg">
-        <div className="space-y-2">
-          <label>Full name</label>
-          <input
-            disabled
-            className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-          />
-        </div>
-
-        <div className="space-y-2">
-          <label>Email address</label>
-          <input
-            disabled
-            className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-          />
-        </div>
-
-        <div className="space-y-2">
-          <div className="flex items-center justify-between">
-            <label htmlFor="nationality">Where are you from?</label>
-            {/* <Image
-              src={countryFlag}
-              alt="Country flag"
-              className="h-5 rounded-sm"
-            /> */}
-          </div>
-          <Suspense fallback={<SpinnerMini />}>
-            <SelectCountry
-              name="nationality"
-              id="nationality"
-              className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
-              defaultCountry={nationality}
-            />
-          </Suspense>
-        </div>
-
-        <div className="space-y-2">
-          <label htmlFor="nationalID">National ID number</label>
-          <input
-            name="nationalID"
-            className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
-          />
-        </div>
-
-        <div className="flex items-center justify-end gap-6">
-          <button className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update profile
-          </button>
-        </div>
-      </form>
+      <UpdateProfileForm>
+        <SelectCountry
+          name="nationality"
+          id="nationality"
+          className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
+          defaultCountry={nationality}
+        />
+      </UpdateProfileForm>
     </div>
   );
 }
