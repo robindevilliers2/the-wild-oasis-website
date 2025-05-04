2fe3f90702a8e0200d95977556ba511824933b65
robindevilliers2 <robindevilliers2@gmail.com>
2025-05-04 12:41:52 +0200

    create and focuse guest session

--- app/account/page.js
+++ app/account/page.js
@@ -7,6 +7,7 @@ export const metadata = {
 export default async function Page() {
   const session = await auth();
   const firstName = session.user.name.split(" ")[0];
+
   return (
     <h2 className="mb-7 text-2xl font-semibold text-accent-400">
       Welcome , {firstName}
