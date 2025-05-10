38a76c1b3294930a374d58c9a892a753a5db8f41
robindevilliers2 <robindevilliers2@gmail.com>
2025-05-10 13:16:52 +0200

    refactor updateReservation to ensure proper data handling and redirect after update

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function deleteReservation(bookingId) {
   const session = await auth();
@@ -59,8 +60,8 @@ export async function updateReservation(formData) {
   if (!session) throw new Error("You must be logged in");
 
   const bookingId = formData.get("bookingId");
-  const num_guests = formData.get("numGuests");
-  const observations = formData.get("observations");
+  const num_guests = Number(formData.get("numGuests"));
+  const observations = formData.get("observations").slice(0, 1000);
 
   const guestBookings = await getBookings(session.user.guestId);
   const guestBookingsIds = guestBookings.map((booking) => booking.id);
@@ -80,4 +81,5 @@ export async function updateReservation(formData) {
   }
 
   revalidatePath(`account/reservations/edit/${bookingId}`);
+  redirect("/account/reservations");
 }
