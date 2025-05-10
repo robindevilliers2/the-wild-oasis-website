5de00f346b33b9818bfc560b51bd0fa2ae8f6337
robindevilliers2 <robindevilliers2@gmail.com>
2025-05-10 11:50:51 +0200

    implement delete reservation functionality with confirmation and loading state

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,26 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+  revalidatePath("/account/reservations");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
